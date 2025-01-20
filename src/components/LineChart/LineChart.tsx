be33810043dc950b91d4276f1192767020f4d776
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-01-20 16:27:56 +0100

    feature(linechart -- basic): Added some default options

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -7,17 +7,37 @@ interface LineChartProps {
 }
 
 export default function LineChart({ data, options = {} }: LineChartProps) {
+    const defaultChartWidth = 750
     const defaultOptions: ApexOptions = {
+        grid: {
+            row: {
+                colors: ['#f3f3f3', 'transparent'],
+                opacity: 0.5,
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
         chart: {
             type: 'line',
+            zoom: {
+                enabled: false,
+            },
+        },
+        stroke: {
+            curve: 'smooth',
         },
     }
+
+    const width = options.chart?.width || defaultChartWidth
+    delete options.chart?.width
 
     return (
         <ReactApexChart
             type='line'
             series={data}
             options={{ ...defaultOptions, ...options }}
+            width={width}
         />
     )
 }
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,6 @@
 export const singleLineSeries: ApexAxisChartSeries = [
     {
         name: 'Series A',
-        data: [28, 29, 33, 36, 32, 32, 33],
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
     },
 ]
