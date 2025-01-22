711de6122cbccfaa635a51757c4a76daac466032
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-01-22 13:52:47 +0100

    test: added cypress for component testing, tested click and hover events

---
+++ src/components/LineChart/test/LineChart.cy.tsx
@@ -0,0 +1,30 @@
+import { categories, singleLineSeries } from '@data/index'
+import LineChart from '../LineChart'
+
+context('LineChart', { viewportWidth: 1280, viewportHeight: 720 }, () => {
+    describe('Interactions', () => {
+        it('Should call the onClick function when passed as input', () => {
+            cy.on('window:alert', (text) => {
+                expect(text).to.eq('Click!')
+            })
+
+            const onClick = () => alert('Click!')
+            cy.mount(
+                <LineChart
+                    data={singleLineSeries}
+                    labels={categories}
+                    onClick={onClick}
+                />
+            )
+            cy.get('svg').should('be.visible')
+            cy.get('svg').realClick()
+        })
+
+        it('Should present a tooltip when hovering on the Chart', () => {
+            cy.mount(<LineChart data={singleLineSeries} labels={categories} />)
+            cy.get('.apexcharts-tooltip').should('not.be.visible')
+            cy.get('svg').realHover()
+            cy.get('.apexcharts-tooltip').should('be.visible')
+        })
+    })
+})
